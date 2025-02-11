e456f2b7cf8ee5695c22d99a09f0ba307bc4c810
s27637 <s27637@pjwstk.edu.pl>
2025-02-11 14:57:17 +0100

    Linked list added

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,3 +1,92 @@
-class LinkedList:
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
     def __init__(self):
-        pass
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+class DNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        new_node = DNode(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+
+    def display_forward(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" <-> ")
+            temp = temp.next
+        print("None")
+
+    def display_backward(self):
+        temp = self.tail
+        while temp:
+            print(temp.data, end=" <-> ")
+            temp = temp.prev
+        print("None")
+
+class CNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = CNode(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = self.head  # Circular link
+            return
+        temp = self.head
+        while temp.next != self.head:
+            temp = temp.next
+        temp.next = new_node
+        new_node.next = self.head  # Circular link
+
+    def display(self):
+        if not self.head:
+            return
+        temp = self.head
+        while True:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+            if temp == self.head:
+                break
+        print("(back to head)")
