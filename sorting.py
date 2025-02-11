5361b03c9a3c40b475b956f24941a87709f4b742
s27637 <s27637@pjwstk.edu.pl>
2025-02-11 14:26:08 +0100

    added insertion sort

--- sorting.py
+++ sorting.py
@@ -15,3 +15,23 @@ def selection_sort(arr):
             if arr[j] < arr[min_index]:
                 min_index = j
         arr[j], arr[min_index] = arr[min_index], arr[i]
+
+'''
+Dominating operation: comparing 2 elements 
+Data size: arr length (n) 
+
+W(n) = Θ(n^2) (when the array is sorted inversely)
+A(n) = Θ(n^2)
+'''
+def insertion_sort(arr):
+    n = len(arr)
+    
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        
+        arr[j+1] = key
